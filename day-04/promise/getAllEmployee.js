c36e8bbd4de8de7ed411b0ca0d9b2c626fb76337
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-07-21 14:49:00 +0300

    fetch-api is ongoing...

--- day-04/promise/getAllEmployee.js
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-const url = "http://localhost:8080/api/employees";
-
-const result = fetch(url)
-            .then(resp => resp.json())
-            .then(data => console.log(data));
-
