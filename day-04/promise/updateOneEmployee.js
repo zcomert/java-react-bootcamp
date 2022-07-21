4824b741c113c3b913a2ac2985621d1ba73366b1
Muhammet Emin ZEYREKLİ <20120205050smu.edu.tr>
2022-07-21 15:58:04 +0300

    updateOneEmployee.js added

---
+++ day-04/promise/updateOneEmployee.js
@@ -0,0 +1,22 @@
+/**
+ * @author M.Emin
+ */
+import fetch from "node-fetch";
+
+function updateOneEmployee() {
+  const url = "http://localhost:8080/api/employees/1";
+  const body = {
+    firstName: "Adem",
+    lastName: "Güneş",
+  };
+  return fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+  }).then((resp) => resp.json());
+}
+const result = await updateOneEmployee(); // await promise in çalışmasının bitmesini bekler.
+console.log(result);
