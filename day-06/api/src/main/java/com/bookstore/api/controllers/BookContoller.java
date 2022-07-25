f84878f00b7324ca03f0e7b09fa4d206abbc9f46
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-07-25 14:14:56 +0300

    BookController has been implemented.

--- day-06/api/src/main/java/com/bookstore/api/controllers/BookContoller.java
+++ day-06/api/src/main/java/com/bookstore/api/controllers/BookContoller.java
@@ -1,48 +1,61 @@
 package com.bookstore.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.api.entities.Book;
+import com.bookstore.api.repositories.BookRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("api/v1/books")
-@Slf4j
+@Slf4j // logger -> log
 public class BookContoller {
 
     // Logger logger = LoggerFactory.getLogger(BookContoller.class.getName());
 
+    private final BookRepository bookRepository;
+
+    public BookContoller(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping
     public List<Book> getAllBooks() {
-        log.info("getAll() has been called.");
-        return null;
+        return bookRepository.findAll();
     }
 
     @PostMapping
-    public Book postOneBook(Book book) {
-        log.info("postOneBook() has been called.");
-        return null;
+    public Book postOneBook(@RequestBody Book book) {
+        return bookRepository.save(book);
     }
 
-    @PutMapping
-    public Book putOneBook(int id, Book book) {
-        log.info("putOneBook() has been called.");
-        return null;
+    @PutMapping(path = "/{id}")
+    public Book putOneBook(@PathVariable(name = "id", required = true) int id, @RequestBody Book book) {
+        Optional<Book> bookOpt = bookRepository.findById(id);
+        if (bookOpt.isPresent()) {
+            book.setId(id);
+            return bookRepository.save(book);
+        }
+        throw new RuntimeException(String.format("Book %s id coluld not found.", id));
     }
 
-    @DeleteMapping
-    public Book deleteOneBook(int id) {
-        log.info("deleteOneBook() has been called.");
-        return null;
+    @DeleteMapping(path = "/{id}")
+    public Book deleteOneBook(@PathVariable int id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            bookRepository.delete(book);
+        }
+        throw new RuntimeException(String.format("Book with %s id could not found.", id));
     }
 }
