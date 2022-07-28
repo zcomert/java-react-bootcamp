b2f03c351efda51aaa5e204fe6f879ee15db4bd1
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-07-28 14:17:22 +0300

    authorlist has been implemented.

--- day-09/bs-store/src/adminpages/authors/ListAuthor.js
+++ day-09/bs-store/src/adminpages/authors/ListAuthor.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ListAuthor(props) {
+export default function ListAuthor({authors}) {
   return (
     <div>
-      Author List {props.authors.length}
+      Author List {authors.length}
 
-      
+      {authors.map((author)=>(
+        <p key={author.id} >{author.id}</p>
+      ))}
       
       <Link to='/admin/authors/add'>Add</Link>
     </div>
