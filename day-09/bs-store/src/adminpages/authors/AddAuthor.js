94823f89894f6736e76c7a6cb0ca9a7586808d8b
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-07-29 15:00:08 +0300

    fab is ongoing...

--- day-09/bs-store/src/adminpages/authors/AddAuthor.js
+++ day-09/bs-store/src/adminpages/authors/AddAuthor.js
@@ -1,29 +1,67 @@
-import React, { useContext } from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
 import AppContext from "../../context/AppContext";
+import AuthorService from "../../services/AuthorService";
 
 export default function AddAuthor() {
 
+  const authorService = new AuthorService();
   const {authors,setAuthors} = useContext(AppContext);
 
+  const initial = {
+    firstName:'',
+    lastName:'',
+    email:''
+  }
+
+  const [form, setForm] = useState(initial); 
+
   const handleClick = () => {
-    const entity = {
-      id:10,
-      firstName:"Ayşe",
-      lastName:"Ay"
-    }
-    setAuthors([...authors, entity])
-   
-    console.log(entity)
-    console.log(authors)
+    // const url = "http://localhost:8080/api/v1/authors";
+    
+    // const entity = {
+    //   firstName:"Ayşe",
+    //   lastName:"Ay",
+    //   email:"abc@gmail.com"
+    // }
+
+    // axios.post(url, entity)
+    // .then(resp => resp.data)
+    // .then(resp => {
+    //   console.log(resp.data);
+    //   setAuthors([...authors, resp.data]) // action -> (dispatch) -> reducer
+    // })
+
+    authorService.postOneAuthor(form).then(resp => 
+      setAuthors([...authors, resp.data]))
+      .catch(err => alert(err));
+  }
+
+  const handleChange = (e) => {
+    // console.log({...form})
+    
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+     })
   }
 
   return (
     <div>
-      AddAuthor
-      <button onClick={handleClick}>Add</button>
-      <div>
-        {authors.length}
-      </div>
+      
+      <input name="firstName" 
+      placeholder="firstname" 
+      onChange={(e) => handleChange(e)}  />
+      
+      <input name="lastName"  placeholder="lastname" 
+      onChange={handleChange}  />
+      
+      <input name="email"  placeholder="email" 
+      onChange={handleChange}  />
+
+      <button onClick={handleClick} >Add Author</button>
+      
+      {/* {JSON.stringify(form)} */}
     </div>
   );
 }
--- day-09/bs-store/src/adminpages/authors/ListAuthor.js
+++ day-09/bs-store/src/adminpages/authors/ListAuthor.js
@@ -1,11 +1,13 @@
+import { Fab } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import AppContext from "../../context/AppContext";
 import AddAuthor from "./AddAuthor";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function ListAuthor() {
   const { authors, setAuthors } = React.useContext(AppContext);
-  
+
   const removeAuthor = (id) => {
     // let arr = [];
 
@@ -16,30 +18,45 @@ export default function ListAuthor() {
     // }
     // setAuthors(arr);
 
-
-    let afterRemove = authors.filter(author => author.id!==id)
+    let afterRemove = authors.filter((author) => author.id !== id);
     setAuthors(afterRemove);
-  }
+  };
+
+  const fabStyle = {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  };
   
+  const fab =  {
+      color: 'primary',
+      sx: fabStyle,
+      icon: <AddIcon />,
+      label: 'Add',
+    };
+  
+
   return (
     <div>
       Author List {authors.length}
-      {authors.map((author,index) => {
+      {authors.map((author, index) => {
         const { id, firstName, lastName } = author;
-        return <p 
-        key={index}>
-          {`${id} ${firstName} ${lastName}`}
-          <button onClick={() => removeAuthor(id)} >Remove</button>
-          </p>;
+        return (
+          <p key={index}>
+            {`${id} ${firstName} ${lastName}`}
+            <button onClick={() => removeAuthor(id)}>Remove</button>
+          </p>
+        );
       })}
       {/* <Link to='/admin/authors/add'>Add</Link> */}
-<div>
-  <button onClick={() => setAuthors([])} >Clear all</button>
-</div>
-<div>
-      <AddAuthor />
-
-</div>
+      <div>
+        <button onClick={() => setAuthors([])}>Clear all</button>
+      </div>
+      <div>
+        <Fab sx={fab.sx} aria-label={fab.label} color={fab.color}>
+          {fab.icon}
+        </Fab>
+      </div>
     </div>
   );
 }
