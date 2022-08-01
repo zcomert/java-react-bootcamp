4b2ec3d1894ad8e5248cb8acc463eefa26fbdd98
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-01 14:40:15 +0300

    ListCategory has been designed.

--- day-11/bs-store/src/adminpages/books/ListBook.js
+++ day-11/bs-store/src/adminpages/books/ListBook.js
@@ -4,6 +4,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import ThemeMenu from '../../components/theme/ThemeMenu';
 import { setTheme} from "../../store/actions/settingActions";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 export default function ListBook() {
   
   const {theme, pageSize} = useSelector(state => state.setting)
@@ -14,23 +22,7 @@ export default function ListBook() {
   }
   
   return (
-    <div>Book List 
-      <div>
-      {theme}
-      </div>
-      <div>
-      {pageSize}
-      </div>
-      <div>
-        <Button onClick={handleClick} variant="contained">Change Theme</Button>
-      </div>
-
-      <div>
-        {
-          <ThemeMenu />
-        }
-      </div>
-
-    </div>
+    <>
+    </>
   )
 }
--- day-11/bs-store/src/adminpages/categories/ListCategory.js
+++ day-11/bs-store/src/adminpages/categories/ListCategory.js
@@ -1,21 +1,56 @@
-import React,{useEffect} from 'react'
-import {useSelector, useDispatch} from "react-redux";
-import {getAllCategories} from "../../store/actions/categoryActions";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllCategories } from "../../store/actions/categoryActions";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button, ButtonGroup } from "@mui/material";
 
 export default function ListCategory() {
-  const {categories} = useSelector(state => state.category);
+  const { categories } = useSelector((state) => state.category);
   const categoryDispatch = useDispatch();
 
   useEffect(() => {
-    categoryDispatch(getAllCategories())
-  },[]);
+    categoryDispatch(getAllCategories());
+  }, []);
 
   return (
-    <div>
-      ListCategory
-      
-      <div>{categories.length}</div>
-     
-    </div>
-  )
+    <>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Category Name</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {categories.map((category) => {
+              const { id, categoryName, description } = category;
+              return (
+                <TableRow key={id}>
+                  <TableCell>{id}</TableCell>
+                  <TableCell>{categoryName}</TableCell>
+                  <TableCell>{description}</TableCell>
+                  <TableCell>
+                    <ButtonGroup orientation="vertical" >
+                      <Button>Edit</Button>
+                      <Button>Remove</Button>
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
