7884167a38641a1fab31fa7fe88d29387edde281
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-02 14:35:08 +0300

    Author store has been implemented.

---
+++ day-12/bs-store/src/store/reducers/authorReducer.js
@@ -0,0 +1,49 @@
+import { DELETE_ONE_AUTHOR, GET_ALL_AUTHORS, GET_ONE_AUTHOR, POST_ONE_AUTHOR, PUT_ONE_AUTHOR } from "../actions/authorActions";
+import { authors } from "../initialValues/authorItems";
+
+const initialState = {
+  authors
+};
+
+export default function authorReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_ALL_AUTHORS:
+      return {
+        ...state,
+        authors: payload,
+      };
+    
+    case GET_ONE_AUTHOR:{
+      return{
+        ...state,
+        author : payload
+      }
+    }
+
+    case POST_ONE_AUTHOR:{
+      return{
+        ...state,
+        authors:[...state.authors, payload]
+      };
+    }
+
+    case PUT_ONE_AUTHOR:{
+      return{
+        ...state,
+        authors:[...state.authors, payload]
+      };
+    }
+
+    case DELETE_ONE_AUTHOR:{
+      return{
+        ...state,
+        authors:[...state.authors.filter(author => author.id!==payload)]
+      };
+    }
+
+    default:
+      return {
+        ...state
+      }
+  }
+}
--- day-12/bs-store/src/store/rootReducer.js
+++ day-12/bs-store/src/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import settingReducer from "./reducers/settingReducer";
 import categoryReducer from "./reducers/categoryReducer";
 import bookReducer from "./reducers/bookReducer";
+import authorReducer from "./reducers/authorReducer";
 
 const rootReducer = combineReducers({
     setting: settingReducer,
     category : categoryReducer,
-    book : bookReducer
+    book : bookReducer,
+    author : authorReducer
 });
 
 export default rootReducer;
