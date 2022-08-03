bbe584d8d33c77c4f0dc6e942cdc80b7fbeae578
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-03 14:17:33 +0300

    Permission-based authorization has been implemented.

--- day-13/api/src/main/java/com/bookstore/api/config/ApplicationSecurityConfig.java
+++ day-13/api/src/main/java/com/bookstore/api/config/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import lombok.RequiredArgsConstructor;
 import static com.bookstore.api.security.ApplicationUserRole.*;
+import static com.bookstore.api.security.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
@@ -29,8 +30,11 @@ public class ApplicationSecurityConfig
                                 .csrf().disable()
                                 .authorizeRequests()
                                 .antMatchers(HttpMethod.GET, "/api/v1/books")
-                                .hasAnyRole(ADMIN.name(), EDITOR.name(), USER.name())
-                                .antMatchers(HttpMethod.POST, "/api/v1/books").hasAnyRole(ADMIN.name())
+                                .hasAuthority(BOOK_GET.getPermission())
+                                // .hasAnyRole(ADMIN.name(), EDITOR.name(), USER.name())
+                                .antMatchers(HttpMethod.POST, "/api/v1/books")
+                                .hasAuthority(BOOK_POST.getPermission())
+                                // .hasAnyRole(ADMIN.name())
                                 .anyRequest()
                                 .authenticated()
                                 .and()
@@ -43,18 +47,21 @@ public class ApplicationSecurityConfig
                 UserDetails admin = User.builder()
                                 .username("admin")
                                 .password(passwordEncoder.encode("admin123456"))
+                                .authorities(ADMIN.getGrantedAuthorities())
                                 // .roles(ADMIN.name())
                                 .build();
 
                 UserDetails editor = User.builder()
                                 .username("editor")
                                 .password(passwordEncoder.encode("editor123456"))
+                                .authorities(EDITOR.getGrantedAuthorities())
                                 // .roles(EDITOR.name())
                                 .build();
 
                 UserDetails user = User.builder()
                                 .username("user")
                                 .password(passwordEncoder.encode("user123456"))
+                                .authorities(USER.getGrantedAuthorities())
                                 // .roles(USER.name())
                                 .build();
 
