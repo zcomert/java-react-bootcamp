b7764e3c3e6ebbcf0c62d7b63ab4a6400005802b
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-04 11:34:43 +0300

    RoleService and RoleController have been done.

---
+++ day-14/api/src/main/java/com/bookstore/api/services/RoleServiceImp.java
@@ -0,0 +1,31 @@
+package com.bookstore.api.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bookstore.api.entities.Role;
+import com.bookstore.api.entities.models.ApiResponse;
+import com.bookstore.api.repositories.RoleRepository;
+import com.bookstore.api.services.Abstract.RoleService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImp implements RoleService {
+    private final RoleRepository roleRepository;
+
+    @Override
+    public ApiResponse<List<Role>> getAllRoles() {
+        var roles = roleRepository.findAll();
+        return ApiResponse.default_OK(roles);
+    }
+
+    @Override
+    public ApiResponse<Role> postOneRole(Role role) {
+        roleRepository.save(role);
+        return ApiResponse.default_CREATED(role);
+    }
+
+}
