1184044d6c058356fae8faaa945c8da9ffa52710
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-04 10:52:38 +0300

    UserServiceImp->Post has been done.

--- day-14/api/src/main/java/com/bookstore/api/services/Abstract/UserService.java
+++ day-14/api/src/main/java/com/bookstore/api/services/Abstract/UserService.java
@@ -1,5 +1,23 @@
 package com.bookstore.api.services.Abstract;
 
+import java.util.List;
+
+import com.bookstore.api.entities.User;
+import com.bookstore.api.entities.models.ApiResponse;
+
 public interface UserService extends ApplicationUserDao {
-    
+
+    ApiResponse<List<User>> getAllUsers();
+
+    ApiResponse<User> getOneUser(int id);
+
+    ApiResponse<User> postOneUser(User user);
+
+    ApiResponse<User> putOneUser(int id, User user);
+
+    User getOneUserByUserName(String username);
+
+    void deleteOneUser(int id);
+
+    User saveOneUser(User user);
 }
--- day-14/api/src/main/java/com/bookstore/api/services/UserServiceImp.java
+++ day-14/api/src/main/java/com/bookstore/api/services/UserServiceImp.java
@@ -1,14 +1,19 @@
 package com.bookstore.api.services;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.api.entities.Role;
 import com.bookstore.api.entities.User;
+import com.bookstore.api.entities.models.ApiResponse;
+import com.bookstore.api.exceptions.notFoundExceptions.UserNotFoundException;
+import com.bookstore.api.repositories.RoleRepository;
 import com.bookstore.api.repositories.UserRepository;
 import com.bookstore.api.security.ApplicationUser;
 import com.bookstore.api.services.Abstract.UserService;
@@ -21,6 +26,8 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImp implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
@@ -61,6 +68,58 @@ public class UserServiceImp implements UserService {
                         true));
 
         return applicationUser;
+    }
+
+    @Override
+    public ApiResponse<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ApiResponse.default_OK(users);
+    }
+
+    @Override
+    public ApiResponse<User> getOneUser(int id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        return ApiResponse.default_OK(user);
+    }
+
+    @Override
+    public ApiResponse<User> postOneUser(User user) {
+        // passwordEncode
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Role: varsayılan olarak USER role atayalım.
+        Set<Role> roles = new HashSet<>();
+        Role role = roleRepository.findByName("USER");
+        roles.add(role);
+        user.setRoles(roles);
+        userRepository.save(user);
+        return ApiResponse.default_OK(user);
+    }
+
+    @Override
+    public ApiResponse<User> putOneUser(int id, User user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User getOneUserByUserName(String username) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteOneUser(int id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public User saveOneUser(User user) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
