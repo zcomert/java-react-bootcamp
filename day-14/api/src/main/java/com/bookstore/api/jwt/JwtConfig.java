14396969e16c2bb0718ac4226a33a177c28250d3
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-04 14:58:11 +0300

    JwtSecretKey has been added to the project.

---
+++ day-14/api/src/main/java/com/bookstore/api/jwt/JwtConfig.java
@@ -0,0 +1,23 @@
+package com.bookstore.api.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "application.jwt")
+@Data
+public class JwtConfig {
+    private String secretKey;
+    private String tokenPrefix;
+
+    @Value("${application.jwt.expire.in}")
+    private Long expireIn;
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+}
