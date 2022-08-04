1be8a11140d1f312997c9406dd30b3a3b93540e5
Zafer CÖMERT <zafer.comert@hotmail.com>
2022-08-04 12:25:12 +0300

    UserController post

--- day-14/api/src/main/java/com/bookstore/api/controllers/UserController.java
+++ day-14/api/src/main/java/com/bookstore/api/controllers/UserController.java
@@ -2,9 +2,12 @@ package com.bookstore.api.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bookstore.api.entities.User;
 import com.bookstore.api.repositories.UserRepository;
 import com.bookstore.api.services.Abstract.ApplicationUserDao;
 import com.bookstore.api.services.Abstract.UserService;
@@ -15,14 +18,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
 public class UserController {
-    
-    private UserService userService;  // UserServiceImp
+
+    private final UserService userService; // UserServiceImp
 
     @GetMapping
-    public ResponseEntity<?> getAllUsers(){
+    public ResponseEntity<?> getAllUsers() {
         var response = userService.getAllUsers();
         return ResponseEntity
-        .status(response.getHttpStatus())
-        .body(response);
+                .status(response.getHttpStatus())
+                .body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postOneUser(@RequestBody User user) {
+        var response = userService.postOneUser(user);
+        return ResponseEntity
+                .status(response.getHttpStatus())
+                .body(response);
     }
 }
