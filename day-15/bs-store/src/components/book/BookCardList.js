49cb9c5a0798d53e54c5e7ff36f33f37fd6a9cc8
Oğuzhan Bilgin <ogzhnbxx@gmail.com>
2022-08-07 14:34:14 +0300

    User BookCardList fetch problem has been fixed.

--- day-15/bs-store/src/components/book/BookCardList.js
+++ day-15/bs-store/src/components/book/BookCardList.js
@@ -1,13 +1,20 @@
 import Grid from "@mui/material/Grid";
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllBooks } from "../../store/actions/bookActions";
 import BookCard from "./BookCard";
 
 export default function BookCardList() {
   const { books } = useSelector((state) => state.book);
+  const bookDispatch = useDispatch();
+
+  useEffect(() => {
+    bookDispatch(getAllBooks());
+  }, []);
+
   return (
     <div>
-      <Grid sx={{mt:3, padding: 2}} container spacing={3}>
+      <Grid sx={{ mt: 3, padding: 2 }} container spacing={3}>
         {books?.map((book) => (
           <Grid spacing={3} item xs={8} md={5} lg={4}>
             <BookCard key={book.id} book={book} />
